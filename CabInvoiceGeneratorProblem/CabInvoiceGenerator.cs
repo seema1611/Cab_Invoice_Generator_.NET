c6c9dae699d399a50138be87d6e6fff879f8dcc7
seema1611 <seemarajpure16@gmail.com>
2020-08-18 01:10:12 +0700

    [Seema]ADDED: feature to return fare for given distance and time

---
+++ CabInvoiceGeneratorProblem/CabInvoiceGenerator.cs
@@ -0,0 +1,26 @@
+﻿namespace CabInvoiceGeneratorProblem
+{
+    using System;
+
+    /// <summary>
+    /// Create Cab Invoice Generator Class.
+    /// </summary>
+    public class CabInvoiceGenerator
+    {
+        private static readonly double MINIMUM_COST_PER_KILOMETER = 10.0;
+        private static readonly int COST_PER_TIME = 1;
+        private static readonly double MINIMUM_FARE = 5.0;
+
+        /// <summary>
+        /// Create Calculate Fare Method.
+        /// </summary>
+        /// <param name="distance"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public double CalculateFare(double distance, int time)
+        {
+            double premiumTotalFare = (distance * MINIMUM_COST_PER_KILOMETER) + (time * COST_PER_TIME);
+            return Math.Max(premiumTotalFare, MINIMUM_FARE);
+        }
+    }
+}
---
+++ CabInvoiceGeneratorProblemTest/CabInvoiceGeneratorTest.cs
@@ -0,0 +1,31 @@
+namespace CabInvoiceGeneratorProblemTest
+{
+    using CabInvoiceGeneratorProblem;
+    using NUnit.Framework;
+
+    public class CabInvoiceGeneratorTest
+    {
+        private CabInvoiceGenerator cabInvoiceGenerator;
+
+        /// <summary>
+        /// setUp Method For Initializing Instance of Class For Test Methods .
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.cabInvoiceGenerator = new CabInvoiceGenerator();
+        }
+
+        /// <summary>
+        /// Create Test For Calculate Total Fare.
+        /// </summary>
+        [Test]
+        public void GivenDistanceAndTime_WhenMorethanFive_ShouldReturnTotalFare()
+        {
+            double distance = 2.0;
+            int time = 5;
+            double fare = this.cabInvoiceGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(25, fare);
+        }
+    }
+}
